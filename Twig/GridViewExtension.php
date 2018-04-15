e4f06d8b4beb1105813697cef11af0dc7cd43fd4
billizzard <billizzard@mail.ru>
2018-04-15 18:29:27 +0300

    gix

---
+++ Twig/GridViewExtension.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace HVF\GridView\Twig;
+
+
+use Twig\Extension\AbstractExtension;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class GridViewExtension extends \Twig_Extension
+{
+    private $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        //$this->requestStack = $requestStack;
+    }
+
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction(
+                'grid_view',
+                array($this, 'gridView'),
+                array('needs_environment' => true)
+            ),
+        );
+    }
+
+    public function gridView(\Twig_Environment $environment, $models)
+    {
+        echo $environment->render('Templates/table.html.twig', ['models' => $models]);
+    }
+}
